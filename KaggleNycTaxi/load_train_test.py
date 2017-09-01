8729dc396fd43ca24e8001fa7433fa263ea21d5c
deo1 <deotsone@gmail.com>
2017-08-31 20:28:13 -0700

    taxi_net: first submission (0.49)

--- KaggleNycTaxi/load_train_test.py
+++ KaggleNycTaxi/load_train_test.py
@@ -3,6 +3,7 @@
 # feature analysis: https://www.kaggle.com/headsortails/nyc-taxi-eda-update-the-fast-the-curious
 
 from taxinet import TaxiNet
+import torch
 import pandas as pd
 import numpy as np
 from datetime import datetime
@@ -55,9 +56,6 @@ combined['minute'] = combined['pickup_datetime'].apply(lambda dt: dt.minute)
 # finally drop the datetime object which won't be useful for training
 combined.drop('pickup_datetime', axis=1, inplace=True)
 
-# trim "id" off the front of the id string column and make it an int type
-combined['id'] = combined['id'].apply(lambda id: int(id[2:]))
-
 # round latitude/longitude to a practical precision. 5 decimal places == 1 meter
 # (should not affect loss estimate but should improve training)
 combined['dropoff_latitude'] = combined['dropoff_latitude'].apply(lambda loc: round(loc, 5))
@@ -70,8 +68,8 @@ combined['pickup_longitude'] = combined['pickup_longitude'].apply(lambda loc: ro
 R = 6373000 # approximate radius of earth in meters
 rad = lambda coor: radians(abs(coor))
 a = lambda lat1, lon1, lat2, lon2: \
-    sin((rad(lat2) - rad(lat1)) / 2)**2 + \
-    cos(rad(lat1)) * cos(rad(lat2)) * sin((rad(lon2) - rad(lon1)) / 2)**2
+        sin((rad(lat2) - rad(lat1)) / 2)**2 + \
+        cos(rad(lat1)) * cos(rad(lat2)) * sin((rad(lon2) - rad(lon1)) / 2)**2
 c = lambda a: 2 * atan2(sqrt(a), sqrt(1 - a))
 distance = lambda lat1, long1, lat2, lon2: round(R * c(a(lat1, long1, lat2, lon2)), 1)
 
@@ -87,9 +85,10 @@ combined['crows_distance'] = combined.apply(
 max_distance = 100 * 10**3  # 100 km
 max_duration = 12 * 60 * 60 # 12 hours
 combined = combined[
-      (combined['set'] == 'train')
-    & (combined['crows_distance'] <= max_distance)
-    & (combined['trip_duration'] <= max_duration)]
+     (combined['set'] == 'test') |
+    ((combined['set'] == 'train') &
+     (combined['crows_distance'] <= max_distance) &
+     (combined['trip_duration'] <= max_duration))]
 
 # TODO : see if loss is lognormal, convert for training then back for scoring
 
@@ -101,34 +100,48 @@ epochs = 20                                  # number of passes across the train
 train = combined[combined['set'] == 'train'] # filter back down to train rows
 exclude = ['id', 'set']                      # we won't use these columns for training
 loss_column = 'trip_duration'                # this is what we're trying to predict
-batch_size = 1024                            # number of samples trained per pass
+batch_size = 2**12                           # number of samples trained per pass
 feature_count = len([col for col in train.columns if col not in exclude and col != loss_column])
-taxi_net = TaxiNet(feature_count, learn_rate=0.0002, cuda=True) # instantiate the neural net graph
+taxi_net = TaxiNet(feature_count, learn_rate=0.002, cuda=True) # instantiate the neural net
 
 for epoch in range(epochs):
-    for batch_idx, batch_x, batch_y in taxi_net.get_batches(train, loss_column, batch_size=1024, exclude=exclude):
+    for batch_idx, batch_x, batch_y in taxi_net.get_batches(train, loss_column, batch_size=batch_size, exclude=exclude):
         
         # Forward pass then backward pass
         # TODO : custom loss function matching the Kaggle requirements
         # TODO : convolutional layers on the coordinates
+        # TODO : cross validation
         output = taxi_net(batch_x)
         loss = taxi_net.learn(output, batch_y)
         
-        print('\rLoss: {:.3f} after {} batches ({:.1f}%), {} epochs. (max(y): {:.1f}){}'.format(
+        print('\rLoss: {:.3f} after {} batches ({:.1f}%), {} epochs. (med(y): {:.1f}){}'.format(
                 loss.data[0],                                  # iteration loss
                 batch_idx,                                     # iteration count
                 100 * batch_idx * batch_size / train.shape[0], # % complete within epoch
                 epoch,                                         # epoch count
-                output.max().data[0],                          # to monitor that the weights haven't saturated to 0
+                output.median().data[0],                       # to monitor that the weights haven't saturated to 0
                 "       "), end="")
 
+    # score and train on the whole set to see where we're at
+    _, all_x, all_y = next(taxi_net.get_batches(train, loss_column, batch_size=train.shape[0], exclude=exclude))
+    print('\nLoss: {:.3f} after {} epochs'.format(taxi_net.loss_function(taxi_net(all_x), all_y).data[0], epoch))
+    
     # shuffle the data so that new batches / orders are used in the next epoch
     train = train.sample(frac=1).reset_index(drop=True)
-    print('\nLoss: {:.3f} after {} epochs'.format(loss.data[0], epoch))
 
 
 # ==============================================
 # Produce estimates for the test set
 # ==============================================
 
+test = combined[combined['set'] == 'test'] # filter back down to test rows
+_, test_x, test_y = next(taxi_net.get_batches(test, loss_column, batch_size=test.shape[0], exclude=exclude))
+test[loss_column] = taxi_net(test_x).cpu().numpy()
+
+# make all trip durations >= 0
+test[loss_column] = test[loss_column].apply(lambda loss: max(0, loss))
+
+test_out = test[['id', loss_column]]
+test_out.to_csv('./data/submission_{}.csv'.format(datetime.strftime(datetime.utcnow(),"%Y-%m-%d-%H-%M-%S")), sep = ',', index = None)
+torch.save(taxi_net.state_dict(), './models/submission_{}.nn'.format(datetime.strftime(datetime.utcnow(),"%Y-%m-%d-%H-%M-%S")))
 # TODO
--- KaggleNycTaxi/taxinet.py
+++ KaggleNycTaxi/taxinet.py
@@ -8,16 +8,22 @@ class TaxiNet(nn.Module):
     def __init__(self, input_nodes, learn_rate=0.1, cuda=False):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Linear(input_nodes, 10), nn.LeakyReLU(),
-            nn.Linear(10, 5), nn.ReLU(),
-            nn.Linear(5, 3), nn.ReLU(),
-            nn.Linear(3, 1), nn.ReLU())
+            nn.Linear(input_nodes, 7), nn.PReLU(7),
+            nn.Linear(7, 3), nn.PReLU(3),
+            nn.Linear(3, 1), nn.PReLU())
+
+        # initialize weights
+        for m in self.model:
+            if isinstance(m, torch.nn.modules.linear.Linear):
+                nn.init.kaiming_normal(m.weight)
+        
+        self.cuda = cuda
+        self.loss_function = nn.L1Loss()
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=learn_rate)
+
         if cuda:
             self.model = self.model.cuda()
-
-        self.cuda = cuda
-        self.loss_function = nn.SmoothL1Loss()
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=learn_rate)
+            self.loss_function == self.loss_function.cuda()
 
     def forward(self, x):
         return self.model(x)
@@ -39,9 +45,11 @@ class TaxiNet(nn.Module):
         row_count = df.shape[0]
         features = [col for col in df.columns if col not in exclude]
 
+        # for relatively small batches, it's fine to miss some off the end
+        # we'll catch it in the next epoch
         for idx, batch in enumerate(np.arange(0, row_count, batch_size)):
-            x = df[features].values[batch:batch + batch_size - 1]
-            y = df[loss_col].values[batch:batch + batch_size - 1]
+            x = df[features].values[batch:batch + batch_size]
+            y = df[loss_col].values[batch:batch + batch_size]
             x, y = torch.Tensor(x), torch.Tensor(y)
             if self.cuda:
                 yield idx, Variable(x.cuda()), Variable(y.cuda())
