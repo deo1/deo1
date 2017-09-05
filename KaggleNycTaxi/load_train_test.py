a3a726ac0a267774573564698add030c61e53a57
Jesse <deotsone@gmail.com>
2017-09-04 17:25:17 -0700

    Regularization Layers
    TaxiNet (
      (model): Sequential (
        (0): Linear (13 -> 10)
        (1): BatchNorm1d(10, eps=1e-05, momentum=0.05, affine=True)
        (2): PReLU (10)
        (3): Dropout (p = 0.002)
        (4): Linear (10 -> 7)
        (5): BatchNorm1d(7, eps=1e-05, momentum=0.05, affine=True)
        (6): PReLU (7)
        (7): Dropout (p = 0.002)
        (8): Linear (7 -> 3)
        (9): BatchNorm1d(3, eps=1e-05, momentum=0.05, affine=True)
        (10): PReLU (3)
        (11): Dropout (p = 0.002)
        (12): Linear (3 -> 1)
        (13): ReLU ()
      )
      (loss_function): SmoothL1Loss (
      )
    )

--- KaggleNycTaxi/load_train_test.py
+++ KaggleNycTaxi/load_train_test.py
@@ -9,101 +9,118 @@ import numpy as np
 from datetime import datetime
 from math import sin, cos, sqrt, atan2, radians
 
-# ===============================
-# Date extraction
-# ===============================
+# TODO args feature
+RUN_FEATURE_EXTRACTION = False
 
-# read data
-test = pd.read_csv('./data/test.csv')
-train = pd.read_csv('./data/train.csv')
+if (RUN_FEATURE_EXTRACTION):
+    # ===============================
+    # Date extraction
+    # ===============================
+    
+    # read data
+    test = pd.read_csv('./data/test.csv')
+    train = pd.read_csv('./data/train.csv')
+    
+    # label data
+    test['set'] = 'test'
+    train['set'] = 'train'
+    
+    # instantiate the loss column in the test set so that schemas match
+    test['trip_duration'] = np.NaN
+    
+    # union `join='outer'` the train and test data so that encoding can be done holistically
+    # and reset the index to be monotically increasing and distinct
+    combined = pd.concat([test, train], join='outer')
+    combined.set_index([list(range(0, combined.shape[0]))], inplace=True)
+    
+    
+    # ===============================
+    # Feature engineering and cleanup
+    # ===============================
+    
+    # drop unneeded column(s)
+    # store_and_fwd_flag looks meaningless and dropoff_datetime is only available
+    # in the train, and is redundant with trip_duration - pickup_datetime
+    combined.drop(['store_and_fwd_flag', 'dropoff_datetime'], axis=1, inplace=True)
+    
+    # segment datetime into year / month / day / hour columns
+    # this should help training differentiate between e.g. weekends vs. weekdays
+    # first encode string as datetime object
+    combined['pickup_datetime'] = combined['pickup_datetime'].apply(
+        lambda dt: datetime.strptime(dt, '%Y-%m-%d %H:%M:%S'))
+    
+    # then encode the important components as integers (ignoring seconds to keep the training simpler)
+    combined['year'] = combined['pickup_datetime'].apply(lambda dt: dt.year)
+    combined['month'] = combined['pickup_datetime'].apply(lambda dt: dt.month)
+    combined['week'] = combined['pickup_datetime'].apply(lambda dt: dt.week)
+    combined['weekday'] = combined['pickup_datetime'].apply(lambda dt: dt.weekday())
+    combined['hour'] = combined['pickup_datetime'].apply(lambda dt: dt.hour)
+    combined['minute'] = combined['pickup_datetime'].apply(lambda dt: dt.minute)
+    
+    # finally drop the datetime object which won't be useful for training
+    combined.drop('pickup_datetime', axis=1, inplace=True)
+    
+    # round latitude/longitude to a practical precision. 5 decimal places == 1 meter
+    # (should not affect loss estimate but should improve training)
+    combined['dropoff_latitude'] = combined['dropoff_latitude'].apply(lambda loc: round(loc, 5))
+    combined['dropoff_longitude'] = combined['dropoff_longitude'].apply(lambda loc: round(loc, 5))
+    combined['pickup_latitude'] = combined['pickup_latitude'].apply(lambda loc: round(loc, 5))
+    combined['pickup_longitude'] = combined['pickup_longitude'].apply(lambda loc: round(loc, 5))
+    
+    # compute an "as the crow flies" curvature distance to nearest 10th of a meter
+    # https://en.wikipedia.org/wiki/Haversine_formula
+    R = 6373000 # approximate radius of earth in meters
+    rad = lambda coor: radians(abs(coor))
+    a = lambda lat1, lon1, lat2, lon2: \
+            sin((rad(lat2) - rad(lat1)) / 2)**2 + \
+            cos(rad(lat1)) * cos(rad(lat2)) * sin((rad(lon2) - rad(lon1)) / 2)**2
+    c = lambda a: 2 * atan2(sqrt(a), sqrt(1 - a))
+    distance = lambda lat1, long1, lat2, lon2: round(R * c(a(lat1, long1, lat2, lon2)), 1)
+    
+    combined['crows_distance'] = combined.apply(
+        lambda row: distance(
+            row['dropoff_latitude'],
+            row['dropoff_longitude'],
+            row['pickup_latitude'],
+            row['pickup_longitude']),
+        axis=1)
+    
+    # drop suspicious rows from the training data
+    max_distance = 100 * 10**3  # 100 km
+    max_duration = 12 * 60 * 60 # 12 hours
+    combined = combined[
+         (combined['set'] == 'test') |
+        ((combined['set'] == 'train') &
+         (combined['crows_distance'] <= max_distance) &
+         (combined['trip_duration'] <= max_duration))]
+    
+    # TODO : see if loss is lognormal, convert for training then back for scoring
+    
+    combined.to_csv('./data/combined.csv', sep=',', index=None)
 
-# label data
-test['set'] = 'test'
-train['set'] = 'train'
-
-# instantiate the loss column in the test set so that schemas match
-test['trip_duration'] = np.NaN
-
-# union `join='outer'` the train and test data so that encoding can be done holistically
-# and reset the index to be monotically increasing and distinct
-combined = pd.concat([test, train], join='outer')
-combined.set_index([list(range(0, combined.shape[0]))], inplace=True)
-
-
-# ===============================
-# Feature engineering and cleanup
-# ===============================
-
-# drop unneeded column(s)
-# store_and_fwd_flag looks meaningless and dropoff_datetime is only available
-# in the train, and is redundant with trip_duration - pickup_datetime
-combined.drop(['store_and_fwd_flag', 'dropoff_datetime'], axis=1, inplace=True)
-
-# segment datetime into year / month / day / hour columns
-# this should help training differentiate between e.g. weekends vs. weekdays
-# first encode string as datetime object
-combined['pickup_datetime'] = combined['pickup_datetime'].apply(
-    lambda dt: datetime.strptime(dt, '%Y-%m-%d %H:%M:%S'))
-
-# then encode the important components as integers (ignoring seconds to keep the training simpler)
-combined['year'] = combined['pickup_datetime'].apply(lambda dt: dt.year)
-combined['month'] = combined['pickup_datetime'].apply(lambda dt: dt.month)
-combined['week'] = combined['pickup_datetime'].apply(lambda dt: dt.week)
-combined['weekday'] = combined['pickup_datetime'].apply(lambda dt: dt.weekday())
-combined['hour'] = combined['pickup_datetime'].apply(lambda dt: dt.hour)
-combined['minute'] = combined['pickup_datetime'].apply(lambda dt: dt.minute)
-
-# finally drop the datetime object which won't be useful for training
-combined.drop('pickup_datetime', axis=1, inplace=True)
-
-# round latitude/longitude to a practical precision. 5 decimal places == 1 meter
-# (should not affect loss estimate but should improve training)
-combined['dropoff_latitude'] = combined['dropoff_latitude'].apply(lambda loc: round(loc, 5))
-combined['dropoff_longitude'] = combined['dropoff_longitude'].apply(lambda loc: round(loc, 5))
-combined['pickup_latitude'] = combined['pickup_latitude'].apply(lambda loc: round(loc, 5))
-combined['pickup_longitude'] = combined['pickup_longitude'].apply(lambda loc: round(loc, 5))
-
-# compute an "as the crow flies" curvature distance to nearest 10th of a meter
-# https://en.wikipedia.org/wiki/Haversine_formula
-R = 6373000 # approximate radius of earth in meters
-rad = lambda coor: radians(abs(coor))
-a = lambda lat1, lon1, lat2, lon2: \
-        sin((rad(lat2) - rad(lat1)) / 2)**2 + \
-        cos(rad(lat1)) * cos(rad(lat2)) * sin((rad(lon2) - rad(lon1)) / 2)**2
-c = lambda a: 2 * atan2(sqrt(a), sqrt(1 - a))
-distance = lambda lat1, long1, lat2, lon2: round(R * c(a(lat1, long1, lat2, lon2)), 1)
-
-combined['crows_distance'] = combined.apply(
-    lambda row: distance(
-        row['dropoff_latitude'],
-        row['dropoff_longitude'],
-        row['pickup_latitude'],
-        row['pickup_longitude']),
-    axis=1)
-
-# drop suspicious rows from the training data
-max_distance = 100 * 10**3  # 100 km
-max_duration = 12 * 60 * 60 # 12 hours
-combined = combined[
-     (combined['set'] == 'test') |
-    ((combined['set'] == 'train') &
-     (combined['crows_distance'] <= max_distance) &
-     (combined['trip_duration'] <= max_duration))]
-
-# TODO : see if loss is lognormal, convert for training then back for scoring
+else:
+    combined = pd.read_csv('./data/combined.csv')
 
 # ==============================================
 # Train the neural net to estimate trip duration
 # ==============================================
 
-epochs = 20                                  # number of passes across the training data
+epochs = 30                                  # number of passes across the training data
 train = combined[combined['set'] == 'train'] # filter back down to train rows
 exclude = ['id', 'set']                      # we won't use these columns for training
 loss_column = 'trip_duration'                # this is what we're trying to predict
-batch_size = 2**12                           # number of samples trained per pass
+batch_size = 2**15                           # number of samples trained per pass
+                                             # (use big batches when using batchnorm)
 feature_count = len([col for col in train.columns if col not in exclude and col != loss_column])
-taxi_net = TaxiNet(feature_count, learn_rate=0.002, cuda=True) # instantiate the neural net
 
+# instantiate the neural net
+taxi_net = TaxiNet(
+    feature_count,
+    learn_rate=.014,
+    cuda=False,
+    max_output=max_duration)
+
+taxi_net.train() # train mode (learn batchnorm mean/var)
 for epoch in range(epochs):
     for batch_idx, batch_x, batch_y in taxi_net.get_batches(train, loss_column, batch_size=batch_size, exclude=exclude):
         
@@ -124,7 +141,9 @@ for epoch in range(epochs):
 
     # score and train on the whole set to see where we're at
     _, all_x, all_y = next(taxi_net.get_batches(train, loss_column, batch_size=train.shape[0], exclude=exclude))
-    print('\nLoss: {:.3f} after {} epochs'.format(taxi_net.loss_function(taxi_net(all_x), all_y).data[0], epoch))
+    train_y = taxi_net(all_x)
+    train_loss = taxi_net.loss_function(train_y, all_y)
+    print('\nLoss: {:.3f} after {} epochs'.format(train_loss.data[0], epoch))
     
     # shuffle the data so that new batches / orders are used in the next epoch
     train = train.sample(frac=1).reset_index(drop=True)
@@ -136,10 +155,16 @@ for epoch in range(epochs):
 
 test = combined[combined['set'] == 'test'] # filter back down to test rows
 _, test_x, test_y = next(taxi_net.get_batches(test, loss_column, batch_size=test.shape[0], exclude=exclude))
-test[loss_column] = taxi_net(test_x).cpu().numpy()
 
-# make all trip durations >= 0
-test[loss_column] = test[loss_column].apply(lambda loss: max(0, loss))
+# TODO : for some reason this makes the results terrible
+#taxi_net.eval() # test mode (apply  batchnorm)
+
+test_output = taxi_net(test_x)
+
+if taxi_net.cuda:
+    test[loss_column] = test_output.cpu().numpy()
+else:
+    test[loss_column] = test_output.data.numpy()
 
 test_out = test[['id', loss_column]]
 test_out.to_csv('./data/submission_{}.csv'.format(datetime.strftime(datetime.utcnow(),"%Y-%m-%d-%H-%M-%S")), sep = ',', index = None)
--- KaggleNycTaxi/taxinet.py
+++ KaggleNycTaxi/taxinet.py
@@ -5,12 +5,33 @@ from torch.autograd import Variable
 import numpy as np
 
 class TaxiNet(nn.Module):
-    def __init__(self, input_nodes, learn_rate=0.1, cuda=False):
+    def __init__(self, input_nodes, learn_rate=0.01, cuda=False, max_output=float("inf")):
         super().__init__()
+        
+        # TODO: batchnorm layers cause bug with cuda=True
         self.model = nn.Sequential(
-            nn.Linear(input_nodes, 7), nn.PReLU(7),
-            nn.Linear(7, 3), nn.PReLU(3),
-            nn.Linear(3, 1), nn.PReLU())
+            # Layer 1
+            nn.Linear(input_nodes, 10),        # affine
+            nn.BatchNorm1d(10, momentum=0.05), # normalize mean/variance
+            nn.PReLU(10),                      # adative leaky
+            nn.Dropout(p=0.002),               # regularize (small dropout when batchnorm)
+            
+            # Layer 2
+            nn.Linear(10, 7),                  # affine
+            nn.BatchNorm1d(7, momentum=0.05),  # normalize mean/variance
+            nn.PReLU(7),                       # adaptive leaky
+            nn.Dropout(p=0.002),               # regularize (small dropout when batchnorm)
+
+            # Layer 3
+            nn.Linear(7, 3),                   # affine
+            nn.BatchNorm1d(3, momentum=0.05),  # normalize mean/variance
+            nn.PReLU(3),                       # adaptive leaky
+            nn.Dropout(p=0.002),               # regularize (small dropout when batchnorm)
+
+            # Layer 3
+            nn.Linear(3, 1),                   # affine
+            nn.ReLU()                          # final output is [0, oo)
+        )
 
         # initialize weights
         for m in self.model:
@@ -18,7 +39,8 @@ class TaxiNet(nn.Module):
                 nn.init.kaiming_normal(m.weight)
         
         self.cuda = cuda
-        self.loss_function = nn.L1Loss()
+        self.max_output = max_output
+        self.loss_function = nn.SmoothL1Loss()
         self.optimizer = torch.optim.Adam(self.parameters(), lr=learn_rate)
 
         if cuda:
@@ -26,7 +48,7 @@ class TaxiNet(nn.Module):
             self.loss_function == self.loss_function.cuda()
 
     def forward(self, x):
-        return self.model(x)
+        return torch.clamp(self.model(x), max=self.max_output)
 
     def learn(self, output, targets):
         loss = self.loss_function(output, targets)
@@ -51,6 +73,7 @@ class TaxiNet(nn.Module):
             x = df[features].values[batch:batch + batch_size]
             y = df[loss_col].values[batch:batch + batch_size]
             x, y = torch.Tensor(x), torch.Tensor(y)
+
             if self.cuda:
                 yield idx, Variable(x.cuda()), Variable(y.cuda())
             else:
